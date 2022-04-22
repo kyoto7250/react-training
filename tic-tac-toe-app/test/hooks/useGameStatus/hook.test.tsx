e45e7c7d0c6bfaaffed7df45361be3864771028a
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-22 14:31:41 +0900

    adding test for hook

--- tic-tac-toe-app/test/hooks/useGameStatus/hook.test.tsx
+++ tic-tac-toe-app/test/hooks/useGameStatus/hook.test.tsx
@@ -1,8 +1,8 @@
 import {
     useGameStatus
 } from '../../../src/hooks/useGameStatus/hook'
-import { renderHook } from '@testing-library/react-hooks'
-
+import { renderHook, act } from '@testing-library/react-hooks'
+import React from 'react';
 
 
 describe('useGameStatus', () => {
@@ -13,6 +13,40 @@ describe('useGameStatus', () => {
     })
 
     test('check init value', () => {
+        expect(result.current.state).toStrictEqual({
+            "history": [{squares: Array(9).fill(null)}],
+            "xIsNext": true,
+            "stepNumber": 0
+        })
+    })
+
+    test('call handleClick', () => {
+        act(() => {
+            result.current.handleClick(1)
+        })
+
+        expect(result.current.state).toStrictEqual({
+            "history": [{squares: Array(9).fill(null)}, {squares: [null, 'X', null, null, null, null, null, null, null]}],
+            "xIsNext": false,
+            "stepNumber": 1
+        })
+    })
+
+    test('call jumpTo', () => {
+        const mock = jest.fn()
+        beforeEach(() => {
+            const state = {
+                history: [{squares: Array(9).fill(null)}, {squares: [null, 'X', null, null, null, null, null, null, null]}],
+                xIsNext: false,
+                stepNumber: 1
+            }
+            jest.spyOn(React, 'useState').mockImplementationOnce(() => [state, mock])
+        })
+
+        act(() => {
+            result.current.jumpTo(0)
+        })
+
         expect(result.current.state).toStrictEqual({
             "history": [{squares: Array(9).fill(null)}],
             "xIsNext": true,
